c863f918ca839fe03c50213127c0c48ae12227e4
aithy ho <91100494@g.mvusd.net>
2022-02-09 08:24:44 -0800

    Parent class and first test on driver

---
+++ src/AllMyPets.java
@@ -0,0 +1,8 @@
+public class AllMyPets
+{
+    public static void main (String[] args)
+    {
+        myPets testPet =  new myPets(false, "Food" , 2, "Test Pet");
+        System.out.println(testPet.toString());
+    }
+}
